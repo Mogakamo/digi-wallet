9b2dba99f3b22b1e89ad743080c376daed168f49
mogakamo <mogaka.amo254@gmail.com>
2023-03-11 19:57:49 +0300

    [commit]: added the client for the wallet

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiKeyAuthGuard } from './auth/guard/ApiKeyAuthGuard';
+import { ApiKeyAuthGuard } from './auth/guard/ApiKeyAuth.guard';
 
 @UseGuards(ApiKeyAuthGuard)
 @Controller()
