bc052436454999218fb52264eabef0ddb24601f7
Patrick Kabuga <kabugap02@gmail.com>
2023-03-12 04:45:34 +0300

    [chore]: Edited the link

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home | ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
